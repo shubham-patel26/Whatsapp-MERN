463c20fe5cf5e2cd920899073111f8a61994d3da
shubham <shubham763116@gmail.com>
2021-05-15 19:57:33 +0530

    sidebar component

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h2>I am a chat component</h2>
+            
+        </div>
+    )
+}
+
+export default Chat;
